9f82812754c2c71be0f9e38899cde4c6e98911f2
Nisarga5 <124888113+Nisarga5@users.noreply.github.com>
2023-02-11 19:21:24 +0530

    Add files via upload

---
+++ character.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int alpha;
+printf("enter the character:");
+scanf("%c%c",&alpha);
+switch(alpha)
+{
+case 'a':printf("is a vowel");
+        break;
+case 'e':printf("is a vowel");
+        break;
+case 'i':printf("is a vowel");
+        break;
+case 'o':printf("is a vowel");
+        break;
+case 'u':printf("is a vowel");
+        break;
+default:printf("consonate");
+        break;
+        }
+        }
