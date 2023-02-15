24bae4170f017d043a3de917becd53f20343075a
Nisarga5 <124888113+Nisarga5@users.noreply.github.com>
2023-02-15 18:01:22 +0530

    success

---
+++ input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int cid;
+float cr;
+printf("Enter the Course ID and Credit:");
+scanf("%d%f",&cid,&cr);
+printf("Course ID: %d",cid);
+printf("\n Credit:%f",cr);
+return;
+}
